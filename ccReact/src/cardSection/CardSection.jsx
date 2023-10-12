6b4d2f8db816d1c70f967be4e954a35a4741e957
Nicole Garcia <lpurgitoryl@gmail.com>
2023-10-11 22:12:51 -0700

    section bg done

--- ccReact/src/cardSection/CardSection.jsx
+++ ccReact/src/cardSection/CardSection.jsx
@@ -3,8 +3,20 @@ import ThemeAndFormContext from "../store/ThemeAndForm-context.jsx";
 import classes from "./CardSection.module.css";
 
 function CardSection(props) {
+  const [color, setColor] = useState([]);
+  const ctx = useContext(ThemeAndFormContext);
+
+  useEffect(() => {
+    // change bg color when context changes
+    if (ctx.theme == "light") {
+      setColor([classes.section, classes.light].join(" "));
+    } else {
+      setColor([classes.section, classes.dark].join(" "));
+    }
+  }, [ctx.theme]);
+
   return (
-    <section className={classes.section}>
+    <section className={color}>
       <div className={classes.infoWrapper}>{props.children}</div>
     </section>
   );
