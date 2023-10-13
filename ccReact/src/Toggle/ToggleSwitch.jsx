a781f55b20da5935634d61edab8d458649edb15f
Nicole Garcia <lpurgitoryl@gmail.com>
2023-10-12 19:48:24 -0700

    save background theme

--- ccReact/src/Toggle/ToggleSwitch.jsx
+++ ccReact/src/Toggle/ToggleSwitch.jsx
@@ -5,11 +5,25 @@ import ThemeAndFormContext from "../store/ThemeAndForm-context";
 function ToggleSwitch() {
   const ctx = useContext(ThemeAndFormContext);
 
-  const [isChecked, setIsChecked] = useState(false);
+  const [isChecked, setIsChecked] = useState(()=> {
+    let theme = localStorage.getItem("theme");
+    if(theme == null || theme == 'false'){
+      localStorage.setItem("theme", "false");
+      return false;
+    }
+    
+    if(theme == "true"){
+      ctx.setTheme(true);
+      return true
+    }
+  });
 
   function clickHandler(e) {
     setIsChecked(e.target.checked);
     ctx.setTheme(e.target.checked);
+    localStorage.setItem("theme", JSON.stringify(e.target.checked));
+    console.log('CURR THEME');
+    console.log(JSON.stringify(e.target.checked));
   }
   return (
     <div>
