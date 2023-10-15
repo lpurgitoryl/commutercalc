6e7fa5e089ea773580520a3ab818a7626e929103
Nicole Garcia <lpurgitoryl@gmail.com>
2023-10-14 18:18:27 -0700

    time and dist parsed

--- ccReact/src/Stats/Stats.jsx
+++ ccReact/src/Stats/Stats.jsx
@@ -2,10 +2,6 @@ import { useEffect, useContext, useState } from "react";
 import ThemeAndFormContext from "../store/ThemeAndForm-context";
 import isEmpty from "lodash/isEmpty";
 
-function parseCommuteDistance(){
-  return;
-}
-
 function Stats() {
   const ctx = useContext(ThemeAndFormContext);
   const [hour, setHour] = useState("");
@@ -19,24 +15,30 @@ function Stats() {
     if(time.length >=2){
       console.log('hours and mins');
       const h = time[0].split("h")[0];
-      console.log(h);
       setHour(h)
       const m = time[1].split("min")[0];
-      console.log(m);
       setMin(m);
     }
     // deal with just hour
+    else if( time[0].includes("h")){
+      const h = time[0].split("h")[0];
+      setMin("");
+      setHour(h);
+    }
+    // deal with just mins
     else if( time[0].includes("min")){
       const m = time[0].split("min")[0];
-      console.log(m);
       setMin(m);
       setHour("");
     }
-
-    // deal with just mins
     
-    return time;
   }
+
+  function parseCommuteDistance(dist){
+    setMiles(dist.split("mi")[0]);
+    return;
+  }
+  
   useEffect(() => {
     if(isEmpty(ctx.vehicle)){
       console.log("no vehicle data yet for Stats");
@@ -44,10 +46,11 @@ function Stats() {
     }
 
     parseCommuteTime(ctx.commuteTime);
-  }, [ctx.commuteData]);
+    parseCommuteDistance(ctx.commuteDistance);
+  }, [ctx.commuteTime, ctx.commuteDistance]);
 
 
-  return <div>{isEmpty(ctx.vehicle) ? <h1>STATS Waiting</h1> : <h1>STATS RECCIEVED {hour} </h1>}</div>;
+  return <div>{isEmpty(ctx.vehicle) ? <h1>STATS Waiting</h1> : <h1>STATS RECCIEVED {`got ${hour}hours and ${min} mins with a distance of ${miles} miles`}</h1>}</div>;
 }
 
 export default Stats;
