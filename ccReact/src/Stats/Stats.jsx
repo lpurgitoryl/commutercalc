de3355737fc8ef93103c9da900e87240a1394d35
Nicole Garcia <lpurgitoryl@gmail.com>
2023-10-14 18:18:48 -0700

    format

--- ccReact/src/Stats/Stats.jsx
+++ ccReact/src/Stats/Stats.jsx
@@ -8,39 +8,38 @@ function Stats() {
   const [min, setMin] = useState("");
   const [miles, setMiles] = useState("");
 
-  function parseCommuteTime(time){
+  function parseCommuteTime(time) {
     time = time.split(" ");
     console.log(time);
     // deal with hour and minutes
-    if(time.length >=2){
-      console.log('hours and mins');
+    if (time.length >= 2) {
+      console.log("hours and mins");
       const h = time[0].split("h")[0];
-      setHour(h)
+      setHour(h);
       const m = time[1].split("min")[0];
       setMin(m);
     }
     // deal with just hour
-    else if( time[0].includes("h")){
+    else if (time[0].includes("h")) {
       const h = time[0].split("h")[0];
       setMin("");
       setHour(h);
     }
     // deal with just mins
-    else if( time[0].includes("min")){
+    else if (time[0].includes("min")) {
       const m = time[0].split("min")[0];
       setMin(m);
       setHour("");
     }
-    
   }
 
-  function parseCommuteDistance(dist){
+  function parseCommuteDistance(dist) {
     setMiles(dist.split("mi")[0]);
     return;
   }
-  
+
   useEffect(() => {
-    if(isEmpty(ctx.vehicle)){
+    if (isEmpty(ctx.vehicle)) {
       console.log("no vehicle data yet for Stats");
       return;
     }
@@ -49,8 +48,18 @@ function Stats() {
     parseCommuteDistance(ctx.commuteDistance);
   }, [ctx.commuteTime, ctx.commuteDistance]);
 
-
-  return <div>{isEmpty(ctx.vehicle) ? <h1>STATS Waiting</h1> : <h1>STATS RECCIEVED {`got ${hour}hours and ${min} mins with a distance of ${miles} miles`}</h1>}</div>;
+  return (
+    <div>
+      {isEmpty(ctx.vehicle) ? (
+        <h1>STATS Waiting</h1>
+      ) : (
+        <h1>
+          STATS RECCIEVED{" "}
+          {`got ${hour}hours and ${min} mins with a distance of ${miles} miles`}
+        </h1>
+      )}
+    </div>
+  );
 }
 
 export default Stats;
